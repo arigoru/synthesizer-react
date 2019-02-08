d42ff43238af037595a7dcfac10bcb80a6e2e223
Allin Ihor <ui.ethuil@gmail.com>
2019-02-08 19:04:26 +0200

    Updates

---
+++ precache-manifest.f243c4dd2f4e3d000a24df9ccacdab32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6aff7b5d4df7f7b7f82f",
+    "url": "/synthesizer-react/static/js/runtime~main.6aff7b5d.js"
+  },
+  {
+    "revision": "c29a7a1cfa86c7e74752",
+    "url": "/synthesizer-react/static/js/main.c29a7a1c.chunk.js"
+  },
+  {
+    "revision": "1015926caf23046f2ba3",
+    "url": "/synthesizer-react/static/js/1.1015926c.chunk.js"
+  },
+  {
+    "revision": "c29a7a1cfa86c7e74752",
+    "url": "/synthesizer-react/static/css/main.66c39535.chunk.css"
+  },
+  {
+    "revision": "cfe0eadfb6e58d87b7d94dcef72bf076",
+    "url": "/synthesizer-react/index.html"
+  }
+];
